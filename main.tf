bcab4885aa64442f1787aabe23fc56858a628b7f
CracktheDom <heymrdjd@gmail.com>
2023-09-24 20:03:59 +0000

    #11 implement aws provider & create S3 bucket

--- main.tf
+++ main.tf
@@ -4,17 +4,34 @@ terraform {
       source  = "hashicorp/random"
       version = "3.5.1"
     }
+    # https://registry.terraform.io/providers/hashicorp/aws/latest
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.17.0"
+    }
   }
+}
+
+provider "aws" {
+  # Configuration options
 }
 
 provider "random" {
   # Configuration options
 }
 
-# Generate a random string 
+# Generate a random string
+# https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
 resource "random_string" "random_bucket_name" {
-  length  = 16
+  length  = 26
   special = false
+  upper = false
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
+resource "aws_s3_bucket" "wonder_bucket" {
+  bucket = random_string.random_bucket_name.result
+
 }
 
 # Output the generated random string
