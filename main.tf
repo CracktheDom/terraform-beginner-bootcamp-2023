9b6e37ff43a4085d96906633052d5136cc9c03ce
CracktheDom <85846263+CracktheDom@users.noreply.github.com>
2023-09-25 18:44:44 -0500

    Move output block from main.tf to outputs.tf

--- main.tf
+++ main.tf
@@ -11,8 +11,3 @@ resource "aws_s3_bucket" "wonder_bucket" {
   bucket = random_string.random_bucket_name.result
 
 }
-
-# Output the generated random string
-output "random_bucket_name_string" {
-  value = random_string.random_bucket_name.result
-}
