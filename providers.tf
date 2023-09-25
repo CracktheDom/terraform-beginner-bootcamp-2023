4558b8b83c4610f543cea4cecd4535af9353921b
CracktheDom <85846263+CracktheDom@users.noreply.github.com>
2023-09-25 18:42:54 -0500

    Move provider blocks in main.tf to providers.tf

--- providers.tf
+++ providers.tf
@@ -1,1 +1,28 @@
+terraform {
+  cloud {
+    organization = "cloudBouncer"
+    workspaces {
+      name = "terra-house-1"
+    }
+  }
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.5.1"
+    }
+    # https://registry.terraform.io/providers/hashicorp/aws/latest
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.17.0"
+    }
+  }
+}
 
+provider "aws" {
+  # Configuration options
+  region = "us-east-2"
+}
+
+provider "random" {
+  # Configuration options
+}
