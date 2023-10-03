0e64c4e4d0ab6f79039e746638852e3cc75ff173
CracktheDom <heymrdjd@gmail.com>
2023-10-03 21:29:08 +0000

    Comment out variables to allow implementation of custom TF providers

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
-variable "index_html_filepath" {
-  type = string
-}
+# variable "index_html_filepath" {
+#   type = string
+# }
 
-variable "error_html_filepath" {
-  type = string
-}
+# variable "error_html_filepath" {
+#   type = string
+# }
 
-variable "content_version" {
-  type = number
-}
+# variable "content_version" {
+#   type = number
+# }
